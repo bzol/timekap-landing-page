f219f4f5509eb01e1f84a35a7870b24c46e94b5e
Zoltan Bende <bendez95@protonmail.com>
2025-08-19 22:24:29 +0200

    no commit message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PostHogProvider } from 'posthog-js/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,17 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_POSTHOG_KEY}
+      options={{
+        api_host: process.env.REACT_APP_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true,
+        debug: process.env.NODE_ENV === 'development',
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
 
